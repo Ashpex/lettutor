e980bbbdee3b12679e71425b4a085933a1196070
ashpex <ashpex@pm.me>
2022-10-12 16:57:31 +0700

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lettutor/presentation/pages/welcom_screen/welcom_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue, scaffoldBackgroundColor: Colors.white),
+      home: WelcomeScreen(),
+    );
+  }
+}
